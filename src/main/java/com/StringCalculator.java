8e6037ccebab1b23f9c6de9cf3809a1b950da122
source-code-repository <me@anonymous.com>
2020-06-08 15:27:17 +0100

    Clean imports

--- src/main/java/com/StringCalculator.java
+++ src/main/java/com/StringCalculator.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
